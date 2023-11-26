29d0f99872ace015091480aeca021a6e5e1a4a6c
AbdoulayePangnonKOUL <AbdoulayePangnonKOUL@TAL-7G6S793>
2023-11-26 05:32:58 +0100

    first push

---
+++ programme_reseau/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+
+namespace programme_reseau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+          string url ="https://codeavecjonathan.com/res/exemple.html" ;
+            //   string url = "https://codeavecjonathan.com/res/exemple.txt";
+           // string url = "https://codeavecjonathan.com/res/pizza1.Json";
+
+
+          var  webClient = new WebClient();
+          string reponse =  webClient.DownloadString(url);
+
+          Console.WriteLine("Acces au reseau");
+          Console.WriteLine(reponse);   
+
+
+        }
+    }
+}
